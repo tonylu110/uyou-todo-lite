873486797dd7db574a3a6ce02922f5b62629a5fc
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-07-27 18:44:43 +0800

    feat: show time ago

--- src/utils/getTime.ts
+++ src/utils/getTime.ts
@@ -1,12 +1,22 @@
 import moment from 'moment'
+import { useI18n } from 'vue-i18n'
+import { useLocaleTimeAgo } from './useLocaleTimeAgo'
 
 function getTime(time: number): string {
+  const { t } = useI18n()
+
   const nowTime = moment().format('YYYY-MM-DD')
   const useTime = moment(time).format('YYYY-MM-DD')
+
+  const selectedDate = new Date(time)
+  const halfYearAgo = new Date()
+  halfYearAgo.setMonth(halfYearAgo.getMonth() - 6)
+
   if (nowTime === useTime)
-    return moment(time).format('hh:mm A')
+    return t('today') + moment(time).format('hh:mm A')
+  else if (selectedDate > halfYearAgo)
+    return useLocaleTimeAgo(new Date(time)).value
   else
     return moment(time).format('YYYY-MM-DD hh:mm A')
 }
-
 export default getTime
